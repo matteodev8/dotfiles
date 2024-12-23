081af18a959b8abfe15b1523fbdd8f98de4475ec
matteodev8 <me@matteodev.xyz>
2024-12-23 15:41:44 +0100

    add alacritty, env and fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,4 +6,7 @@ set fish_greeting
 starship init fish | source
 export PATH="/home/matteo/.bin:$PATH"
 alias ff="fastfetch"
-alias ls="eza --grid -l"
+# alias ls="eza --grid -l"
+alias ls="eza -l --git --header --hyperlink"
+alias lockdown="echo integrity | sudo tee /sys/kernel/security/lockdown"
+alias rp="realpath"
