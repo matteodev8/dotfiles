8a7fa3067a36d19bdcce69cc38c15c227da7d5fd
matteodev8 <me@matteodev.xyz>
2025-01-19 14:23:31 +0100

    add zellij

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-stows=("fish" "alacritty" "environment" "ssh" "git")
+stows=("fish" "alacritty" "environment" "ssh" "git" "zellij")
 
 for stow in "${stows[@]}"; do
   stow -v -t ~ $stow
