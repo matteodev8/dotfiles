4bc8a3954c8dfdfbac61706161bac1ee89cd05e2
matteodev8 <me@matteodev.xyz>
2025-01-19 14:19:59 +0100

    add icons to ls

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,6 @@ starship init fish | source
 export PATH="/home/matteo/.bin:$PATH"
 alias ff="fastfetch"
 # alias ls="eza --grid -l"
-alias ls="eza -l --git --header --hyperlink"
+alias ls="eza -l --icons --git --header --hyperlink"
 alias lockdown="echo integrity | sudo tee /sys/kernel/security/lockdown"
 alias rp="realpath"
